7b13d6416c3e96b5db21c56a071b5b46f15b80e8
Ricardo Nava <ricardo1725@gmail.com>
2023-04-01 12:27:05 -0400

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+//Defining all my variables
+let num1 = 2;
+let num2 = 31;
+let num3 = 15;
+let num4 = 11;
+const multiply = num1 * num2;
+const mod = num3 % num4;
+// Create a variable called max that finds the highest number in a set; the value returned should be 20.
+let set = [5, 10, 15, 20];
+let max = Math.max(...set);
 
-// Write your code here
+// Create a variable called random that will generate a random integer greater than 0
+let random = Math.floor(Math.random() * (Number.MAX_SAFE_INTEGER - 1)) + 1;
+
+// Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62
+multiply (2,31);
+
+// Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+mod (15,11);
